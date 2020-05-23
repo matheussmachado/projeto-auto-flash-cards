1574ba80ebb7c1965791aac257ba91851b61e5d5
Matheus Machado <>
2020-05-23 17:01:22 -0300

    merge do branch escala2.0

--- src/funcs.py
+++ src/funcs.py
@@ -18,35 +18,3 @@ def get_from_txt(file='frases.txt'):
         else:
             print(f'\n\n"{file}" não é um arquivo ou um path de arquivo válido.')
             file = input('\nInsira um arquivo existente no mesmo path da VENV ou insira um path de arquivo válido: ')
-
-
-"""def anki_bot(autoCards):
-    if type(autoCards) != AutoCards:
-        return
-        
-    url = "https://ankiweb.net/account/login"
-    browser = Firefox()
-    browser.get(url)
-    sleep(3)
-
-    browser.find_element_by_css_selector('input[id="email"]').send_keys(autoCards.email)
-    sleep(2)
-
-    browser.find_element_by_css_selector('input[type="password"]').send_keys(input('senha: '))
-    browser.find_element_by_css_selector('input[type="submit"]').click()
-    sleep(4)
-    #TODO: VALIDAR PELA URL A SEGUIR PARA VERIFICAR SE A SENHA FORNECIDA FOI VALIDA
-
-    #clicar na ancora que adiciona os cards
-    browser.find_elements_by_css_selector('a[class="nav-link"]')[1].click()
-
-    sleep(4)
-
-    for card in autoCards.cards:
-        browser.find_element_by_id('f0').send_keys(card.front)
-        sleep(1)
-        browser.find_element_by_id('f1').send_keys(card.back)
-        sleep(1)
-        browser.find_element_by_css_selector('button[class$="primary"]').click()
-        
-        sleep(1)    """
